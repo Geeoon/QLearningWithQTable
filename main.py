aa6652443429d105cd74f212b798d3b609e3a280
Geeoon Chung <geeoon13@gmail.com>
2022-03-31 05:09:34 -0500

    added function to return list of best actions

--- main.py
+++ main.py
@@ -38,18 +38,18 @@ class Board:
         return self._board.shape
 
     def get_actions(self, x:int, y:int):
-        possible_actions = [1, 2, 3, 4]  # up, right, down, left
+        possible_actions = [0, 1, 2, 3]  # up, right, down, left
         if x == 0:
-            possible_actions.remove(4)
+            possible_actions.remove(3)
 
         if y == 0:
-            possible_actions.remove(1)
+            possible_actions.remove(0)
 
         if x == self._board.shape[0] - 1:
-            possible_actions.remove(2)
+            possible_actions.remove(1)
 
         if y == self._board.shape[1] - 1:
-            possible_actions.remove(3)
+            possible_actions.remove(2)
 
         return possible_actions
 
@@ -58,11 +58,15 @@ class QTable:
     def __init__(self, board:Board):
         self._table = np.zeros(shape=(board.get_shape()[0], board.get_shape()[1], 4), dtype=int)
 
-    def get_state_action_value(self, x:int, y:int):
+    def get_state_action_values(self, x:int, y:int):
         return self._table[x, y]
 
-    def set_state_action_value(self, x:int, y:int, val:float):
-        self._table[x, y] = val
+    def set_state_action_value(self, x:int, y:int, action:int, val:float):
+        self._table[x, y, action] = val
+
+    def get_max_state_action_value_actions(self, x:int, y:int):
+        print(np.argwhere(self.get_state_action_values(x, y) == np.amax(self.get_state_action_values(x, y))).flatten())
+        return np.argwhere(self.get_state_action_values(x, y) == np.amax(self.get_state_action_values(x, y))).flatten()
 
 
 class Agent:
@@ -85,30 +89,36 @@ class Agent:
         return output
 
     def move(self, dir:int):
-        if dir == 1:
+        if dir == 0:
             self._position[1] = self._position[1] - 1
-        elif dir == 2:
+        elif dir == 1:
             self._position[0] = self._position[0] + 1
-        elif dir == 3:
+        elif dir == 2:
             self._position[1] = self._position[1] + 1
-        elif dir == 4:
+        elif dir == 3:
             self._position[0] = self._position[0] - 1
 
     def update(self):
         possible_actions = self._board.get_actions(self._position[0], self._position[1])
         if random.random() < self._learning_rate:  # explore
             self.move(random.choice(possible_actions))
-            return self._board.get_tile(self._position[0], self._position[1]).score
+        else:
+            best_actions = np.intersect1d(possible_actions, self._qtable.get_max_state_action_value_actions(self._position[0], self._position[1]))
+            action = random.choice(best_actions)
+            self.move(action)
+
+        return self._board.get_tile(self._position[0], self._position[1]).score
 
     def episode(self):
         print(self)
         while self._board.get_tile(self._position[0], self._position[1]).terminal == False:
             self.update()
-            print(agent)     
+            print(agent)
+        self._position = [0, 0]
 
 
 if __name__ == "__main__":
     board = Board(5, 5)
     board.set_tile(4, 4, Tile(100, True))
-    agent = Agent(1, 1.0, board)
+    agent = Agent(0, 1.0, board)
     agent.episode()
